a1a0bbd3b1084d872c638f606091ceb117562ba4
PedrogmBR <meloni444@gmail.com>
2024-09-25 18:54:26 +1000

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -1,1 +1,16 @@
-# Abridor-site
+#include <iostream>
+#include <cstdlib> // Para a função system()
+
+int main() {
+    // Substitua o link abaixo pelo website que você deseja abrir
+    std::string url = "https://www.exemplo.com";
+    
+    // Comando para abrir o link no navegador padrão (Windows)
+    std::string command = "start " + url;
+
+    // Executa o comando
+    system(command.c_str());
+
+    return 0;
+}
+
